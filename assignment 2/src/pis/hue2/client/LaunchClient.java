ed842c21d97353f1b494b1a1f5dc58cc61407cd5
RiccardoDotExe <74658601+RiccardoDotExe@users.noreply.github.com>
2021-01-09 13:44:59 +0100

    CON DEL DSC GET für client

--- assignment 2/src/pis/hue2/client/LaunchClient.java
+++ assignment 2/src/pis/hue2/client/LaunchClient.java
@@ -23,15 +23,97 @@ public class LaunchClient
             // keyboard.read line can be replaced and is only here for testing purposes
             String command = keyboard.readLine();
 
+            switch (command)
+            {
+                case "CON":
+                    out.println("CON");
+                    String serverResponseCON = input.readLine();
+                    System.out.println(serverResponseCON);
+                    if(serverResponseCON.equals("ACK")){
+                        System.out.println("Server: " + "Your connection was accepted.");
+                    }
+                    else if(serverResponseCON.equals("DND")){
+                        System.out.println("Server: " + "There was an error trying to connect to the server.");
+                    }
+                    else{
+                        System.out.println("Server: " + "Didn't get your request.");
+                    }
+                    break;
+
+                case "DEL":
+                    out.println("DEL");
+                    String serverResponseDEL = input.readLine();
+                    if(serverResponseDEL.equals("ACK")){
+                        System.out.println("Server: " + "Your delete request was accepted.");
+                    }
+                    else if(serverResponseDEL.equals("DND")){
+                        System.out.println("Server: " + "There was an error trying to delete the file from the server.");
+                    }
+                    else{
+                        System.out.println("Server: " + "Didn't get your request.");
+                    }
+                    break;
+
+                case "DSC":
+                    out.println("DSC");
+                    String serverResponseDSC = input.readLine();
+                    if(serverResponseDSC.equals("DSC")){
+                        System.out.println("Server: " + "You were disconnected successfully.");
+                    }
+                    else{
+                        System.out.println("Server: " + "There was an error while trying to disconnect you.");
+                    }
+                    break;
+
+                case "GET":
+                    out.println("GET");
+                    String serverResponseGET = input.readLine();
+                    if(serverResponseGET.equals("ACK")){
+                        System.out.println("Server: " + "Your get request was accepted.");
+                        out.println("ACK");
+                        String serverResponseGET2 = input.readLine(); // This is the data we wanted to get.
+                        out.println("ACK");
+                    }
+                    else if(serverResponseGET.equals("DND")){
+                        System.out.println("Server: " + "There was an error trying to get the file from the server.");
+                    }
+                    else{
+                        System.out.println("Server: " + "Didn't get your request.");
+                    }
+                    break;
+
+                case "LST":
+                    out.println("LST");
+                    String serverResponseLST = input.readLine();
+                    if(serverResponseLST.equals("DSC")){
+                        System.out.println("Server: " + "You were disconnected successfully.");
+                    }
+                    else{
+                        System.out.println("Server: " + "There was an error while trying to disconnect you.");
+                    }
+                    break;
+
+                case "PUT":
+                    out.println("PUT");
+                    String serverResponsePUT = input.readLine();
+                    if(serverResponsePUT.equals("DSC")){
+                        System.out.println("Server: " + "You were disconnected successfully.");
+                    }
+                    else{
+                        System.out.println("Server: " + "There was an error while trying to disconnect you.");
+                    }
+
+                    break;
+
+                default:
+                    System.out.println("Server didn't respond.");
+                    break;
+
+            }
+
             if (command.equals("quit")) break;
 
-            // sends out to the server rewrite as necessary
-            out.println(command);
 
-            // the server response get it to whichever way is necessary to display the things properly
-            String serverResponse = input.readLine();
-
-            System.out.println("Server: " + serverResponse);
         }
     }
 }
---
+++ assignment 2/src/pis/hue2/client/ServerHandler.java
@@ -0,0 +1,7 @@
+package pis.hue2.client;
+
+import pis.hue2.server.LaunchServer;
+
+public class ServerHandler {
+
+}
